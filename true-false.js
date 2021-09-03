9b0b897e340356db5cebce86e6119ddf2c76f596
AshfatAbegh <mdabegh50@gmail.com>
2021-09-03 23:25:40 +0600

    null vs undefined clear for interview

---
+++ true-false.js
@@ -0,0 +1,24 @@
+// Falsy
+// o
+// ""
+// undefined
+// null 
+// NaN
+// false
+// Truthy
+// "0", " "
+let name= false;
+console.log(name);
+// const name = "Solaiman";
+// if(name.length>0){
+//     console.log("is true");
+// }
+// else{
+//     console.log("false");
+// }
+if(name){
+    console.log("is true");
+}
+else{
+    console.log("is false");
+}
